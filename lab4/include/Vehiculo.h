038d249463b709685f5c89a96ce36f790d582124
Avril <aalbistur27@gmail.com>
2026-06-01 15:36:06 -0300

    Vehiculo.h con detalles para arreglar

--- lab4/include/Vehiculo.h
+++ lab4/include/Vehiculo.h
@@ -1,7 +1,11 @@
 #ifndef VEHICULO_H
 #define VEHICULO_H
 
-#include "TipoVehiculo.h"
+#include "TipoVehiculo.h" // puede ser forward declaration, depende de las otras implementaciones
+#include "DTVehiculosConductor.h"
+#include "Viaje.h"
+#include "Conductor.h"
+#include <set>
 #include <string>
 
 class Vehiculo {
@@ -11,9 +15,23 @@ private:
     std::string marca;
     std::string modelo;
     TipoVehiculo tipo;
+    //puesto que es una relacion bidireccional necesitamos poder acceder al conductor y por otra parte a los viajes
+    Conductor* duenio;
+    std::set<Viaje*> viajes;
 
 public:
     Vehiculo(std::string matricula, int capacidad, std::string marca, std::string modelo, TipoVehiculo tipo);
+    std::string getNicknameConductor();
+    std::set<Viaje*> getViajes();
+    bool existeViaje(int codigo);
+    Viaje* getViaje(int codigo);
+    //std::set<//> obtenerDatosRelacionados();//VER CLASE DE ARRAYS PRIMERO, PUEDE SER QUE NECESITE UN DATATYPE NUEVO
+    // array[codigo,fecha,origen,destino] obtenerDatosViaje(std::string nickname);
+    DTVehiculosConductor getDTVehiculoConductor();
+    int getCapacidad();
+    bool hayViajesConductor(DTFecha fecha);
+    bool hayViajesFecha(DTFecha fecha);
+    void asociarViaje(Viaje* viaje);
     ~Vehiculo();
 };
 
