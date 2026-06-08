759f7ccb449798ab75faf1f91da86aeb1f3b03dc
Lulimartinu <lulimartinucci@hotmail.com>
2026-06-08 19:41:34 -0300

    agrego metodos esConductorDe, getViaje, listarVehiculos, hayViajesFechaConductor a Conductor.cpp

--- lab4/src/Conductor.cpp
+++ lab4/src/Conductor.cpp
@@ -31,3 +31,54 @@ void Conductor::agregarVehiculo(Vehiculo* vehiculo){
     return this->calificacionPromedio;
 }*/
 
+bool Conductor::esConductorDe(int codigo) {
+    // 1.2.1* [foreach] v := next()
+    // Itero por el mapa de vehículos que tiene este conductor
+    for (auto const& [matricula, vehiculo] : this->vehiculos) {
+        if (vehiculo != nullptr) {
+            //viajes := getViajes() -> exists(codigo)
+            // uso metodo ded vehiculo.cpp existeViaje 
+            if (vehiculo->existeViaje(codigo)) { 
+                return true; // true = es el conductor de este viaje para dicho codigo indicado
+            }
+        }
+    }
+    return false; 
+}
+
+// Obtiene el viaje cuyo código se recibe como parámetro recorriendo sus vehículos
+Viaje* Conductor::getViaje(int codigo) {
+    //recorro 
+    for (auto const& [matricula, vehiculo] : this->vehiculos) {
+        if (vehiculo != nullptr && vehiculo->existeViaje(codigo)) {
+            return vehiculo->getViaje(codigo); //  puntero al Viaje original
+        }
+    }
+    return nullptr; // no se encuentra el viaje en ningun vehiculo.
+}
+
+
+void Conductor::listarVehiculos() {
+    //recorro
+    for (auto const& [matricula, vehiculo] : this->vehiculos) {
+        if (vehiculo != nullptr) {
+            DTVehiculosConductor dt = vehiculo->getDTVehiculoConductor();
+            //imprimo los datos del DT ? 
+            // cout .. ??
+        }
+    }
+}
+
+// Verifico si el conductor tiene viajes asociados en una fecha indicda.
+bool Conductor::hayViajesFechaConductor(DTFecha fecha) {
+    //recorro
+    for (auto const& [matricula, vehiculo] : this->vehiculos) {
+        if (vehiculo != nullptr) {
+            // cada vehiculo -> tiene viajes en esa fecha indicada ? 
+            if (vehiculo->hayViajesFecha(fecha)) {
+                return true; 
+            }
+        }
+    }
+    return false;
+}
